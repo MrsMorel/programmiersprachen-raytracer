c29f36187e6195c3aa5d7bb2c16896235a09978e
MrsMorel <xxanne.b.tellexx@gmail.com>
2022-08-30 15:13:53 +0200

    testing renderer with sdf, weird effects

--- framework/renderer.cpp
+++ framework/renderer.cpp
@@ -55,21 +55,18 @@ void Renderer::write(Pixel const& p)
 
 void Renderer::render(const Scene &s) {
 
-    for (unsigned y = 0; y < height_; ++y) {
-        for (unsigned x = 0; x < width_; ++x) {
-            Pixel p(x,y);
-                       // TODO render fertigstellen
-
-            Ray ray = s.camera.c_ray(x , y , width_, height_);
+    for (unsigned y = 0; y < s.rend.height; ++y) {
+        for (unsigned x = 0; x < s.rend.width; ++x) {
+            Pixel p{x,y};
+            Ray ray = s.camera.c_ray(x , y , s.rend.width, s.rend.height);
             p.color = tonemap(trace(ray, s));
             write(p);
-
         }
     }
     ppm_.save(filename_);
 }
 //todo durch kamera generierten strahl mit scene schneiden, hitpoint über lichtquellen beleuchten
-
+//Algorithmus auf Folien
 Color Renderer::trace(const Ray &ray, const Scene &s) {
     std::shared_ptr<Shape> closest_o = nullptr;
     Hitpoint closest_t;
@@ -79,12 +76,15 @@ Color Renderer::trace(const Ray &ray, const Scene &s) {
         /*if (hit.cut){
             std::cout << "hit" << "\n";
         }*/
-        if (hit.cut && hit.t < closest_t.t){
+        if (hit.cut && (hit.t < closest_t.t)){
+            std::cout << "hit" << "\n";
+
             closest_t = hit;
             closest_o = element;
         }
     }
     if (closest_o != nullptr){
+        std::cout << "aaaaa" << "\n";
         return shade(s, closest_o, ray, closest_t);
     } else {
         return s.ambient.color;
@@ -98,33 +98,27 @@ Color Renderer::shade(const Scene &s, std::shared_ptr<Shape> const& sharedPtr, c
     //generate new rays if reflection, refraction or shadows
 
     //ambient
-/*
-    Color c_ambient = calc_ambient(s, sharedPtr, hitpoint);
-    Color c_diffuse = calc_diffuse(s, sharedPtr, hitpoint);
-    Color shading = c_ambient + c_diffuse;
-    return shading;*/
-
-    // other method
+    Color ka_ambient = calc_ambient(s, sharedPtr, hitpoint);
+    Color kd_diffuse;
+    // calc diffuse
     Color kd_total;
-    for (auto const& light : s.light_vector) {
-        auto hitpoint_to_light =
-                Ray{hitpoint.point, glm::normalize(light->pos - hitpoint.point)};
-        auto n_dot_i = glm::clamp(
-                glm::dot(hitpoint.normal, hitpoint_to_light.direction), 0.f, 1.f);
-        bool shadow = false;
-        for (auto const& sh : s.shape_vector) {
+    for (auto const &light: s.light_vector) {
+        auto light_hit = Ray{hitpoint.point, glm::normalize(light->pos - hitpoint.point)};
+        auto n_dot_i = glm::clamp(glm::dot(hitpoint.normal, light_hit.direction), 0.f, 1.f);
+        bool block = false;
+        for (auto const &sh: s.shape_vector) {
             if (sharedPtr == sh)
                 continue;
-            if (sh->intersect(hitpoint_to_light).cut)
-                shadow = true;
+            if (sh->intersect(light_hit).cut)
+                block = true;
         }
-        if (!shadow)
+        if (!block)
+            std::cout << "block" << "\n";
             kd_total += light->color * light->brightness * sharedPtr->material()->kd * n_dot_i;
     }
-
-    return sharedPtr->material()->ka * s.ambient.color  + kd_total;
-
+    return ka_ambient + kd_total;
 }
+
 
 Color Renderer::tonemap(Color const& clr){
     Color color_help{ clr.r / (clr.r + 1.0f) , clr.g / (clr.g + 1.0f) , clr.b / (clr.b + 1.0f)};
@@ -139,6 +133,7 @@ Color Renderer::calc_ambient(const Scene &s, const std::shared_ptr<Shape> &share
 /*
 Color Renderer::calc_diffuse(const Scene &s, const std::shared_ptr<Shape> &sharedPtr, const Hitpoint& hitpoint) {
     //diffuse
+
     Color calc_diffuse;
     Hitpoint hit_help;
     for (auto light : s.light_vector) {
@@ -149,8 +144,22 @@ Color Renderer::calc_diffuse(const Scene &s, const std::shared_ptr<Shape> &share
             //punktlicht
         }
     }
-    return Color();
-}*/
+    return Color();*/
+   /*
+    auto light_hit = Ray{hitpoint.point, glm::normalize(light->pos - hitpoint.point)};
+    auto n_dot_i = glm::clamp(glm::dot(hitpoint.normal, light_hit.direction), 0.f, 1.f);
+    bool shadow = false;
+    for (auto const& sh : s.shape_vector) {
+        if (sharedPtr == sh)
+            continue;
+        if (sh->intersect(light_hit).cut)
+            shadow = true;
+    }
+    if (!shadow)
+        kd_total += light->color * light->brightness * sharedPtr->material()->kd * n_dot_i;
+}
+*/
+/*
 Color Renderer::calc_diffuse( Scene const& scene, std::shared_ptr<Shape> const& shape,Hitpoint const& hit){
     Color diffused_clr{0.0f,0.0f,0.0f};
     std::vector<Color> result;
@@ -187,64 +196,11 @@ Color Renderer::calc_diffuse( Scene const& scene, std::shared_ptr<Shape> const& 
             //std::cout << std::endl;
         }
     }
-    //std::cout << "the Vec has this many elements: " << result.size() << std::endl;
-    //std::cout << "first element: " << result[0] << std::endl;
-    for(auto clr : result){
-        //Color clamp_clr = clamping(clr);
-        //diffused_clr += clamp_clr;
-    }
-    //std::cout << diffused_clr << std::endl;
-    //std::cout << "" << std::endl;
     return diffused_clr;
 }
 
+*/
 
-/*
-Color Renderer::calculateSpecular(std::shared_ptr<Shape> const& shape, Scene const& scene, HitPoint const& hit){
-    Color spec_clr{0.0f,0.0f,0.0f};
-    std::vector<Color> calc_clrs;
-
-    for(auto light : scene.light_vec){
-        bool obstacle = false;
-        HitPoint light_hit;
-
-        glm::vec3 vec_lights{light->pos_ - hit.intersect_pt_};
-        Ray ray_lights{hit.intersect_pt_ + 0.1f * hit.normal_,glm::normalize(vec_lights)}; //checks if obstacle is between Light and intersection
-
-        for(auto i : scene.shape_vec){
-            light_hit = i->intersect(ray_lights);
-            if(light_hit.intersection_){
-                obstacle = true;
-            }
-        }
-
-        if(obstacle){
-            float m = shape->getMat()->m_;
-            glm::vec3 r = 2.0f*glm::dot(hit.normal_,glm::normalize(vec_lights))*hit.normal_-glm::normalize(vec_lights); //glm::dot -> Skalarprodukt
-            //printVec(r);
-            glm::vec3 v = glm::normalize(scene.camera_.pos_ - hit.intersect_pt_);
-            //printVec(v);
-            //std::cout << std::endl;
-            float cross_prod = glm::dot(r,v);
-            cross_prod = std::max(cross_prod, 0.0f);
-            if(cross_prod < 0){
-                cross_prod = -cross_prod;}
-            Color ks = shape->getMat()->ks_;
-            Color ip{light->color_*light->brightness_};
-            float cos = pow(cross_prod,m);
-            float m_2 = (m+2)/(2*M_PI);
-            calc_clrs.push_back({((ks*ip)*cos)*m_2});
-        }
-
-    }
-
-    //std::cout << calc_clrs.size();
-    for(auto clr : calc_clrs){
-        Color clamp_clr = {clamping(clr)};
-        spec_clr += clamp_clr;
-    }
-    return spec_clr;
-}*/
 
 
 
--- source/raytracer.cpp
+++ source/raytracer.cpp
@@ -5,15 +5,10 @@
 #include <cmath>
 #include "renderer.hpp"
 #include "scene.hpp"
-//#include "scene.hpp"
 
 //now single threaded again
 int main(int argc, char* argv[])
 {
-  unsigned const image_width = 800;
-  unsigned const image_height = 600;
-  std::string const filename = "./checkerboard.ppm";
-
   //Scene s = sdfParser("./example1.sdf");
   //std::cout << s.material_map.empty() << "\n";
   //std::cout << s.material_map.begin()->second << "\n";
@@ -22,22 +17,19 @@ int main(int argc, char* argv[])
         std::cout << element.first << "\n";
     }
 */
-  /*Renderer renderer{image_width, image_height, filename};
-  renderer.render(s);
-*/
 
-  auto scene = sdfParser("../source/example2.sdf");
+  auto scene = sdfParser("../source/test.sdf");
         Renderer renderer{scene.rend.width, scene.rend.height, scene.rend.filename};
         renderer.render(scene);
-        /*
-Window window{{image_width, image_height}};
+
+    Window window{{scene.rend.width, scene.rend.height}};
 
   while (!window.should_close()) {
     if (window.get_key(GLFW_KEY_ESCAPE) == GLFW_PRESS) {
       window.close();
     }
     window.show(renderer.color_buffer());
-  }*/
+  }
 
   return 0;
 }
